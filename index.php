75850237ca9930f90a6f02aa663f6eae6037449d
Evgeny <helpme228@rambler.ru>
2021-01-03 14:36:10 +0400

    New commit php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Лабораторная работа №1</title>
+<link rel = "stylesheet" href = "mycss.css">
+<link rel = "shortcut" href = "favicon,ico">
+</head>
+<body>
+<div class = "mydiv1">
+<?php 
+echo "Площадь круга с радиусом 5 =";
+echo 5*5*3.14;
+?>
+</div>
+</body>
+</html>
